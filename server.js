87a90b8c4baa406474600e19a252fc198dac3445
raviwankhade1907 <raviwankhade786@gmail.com>
2022-08-01 18:15:03 +0530

    fourth time

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ var express=require('express');
 var app=express();
 
 app.get("/",(req, res)=>{
-    res.send(
-       index.html
-    );
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get("/aboutus",(req, res)=>{
-    res.send(
-       aboutus.html
-    );
+    res.sendFile(path.join(__dirname, '/aboutus.html'));
 });
 
 
 app.get("/contact",(req, res)=>{
-    res.send(
-       contact.html
-    );
+    res.sendFile(path.join(__dirname, '/contact.html'));
 });
 
 var server=app.listen(3000);
